7c3db6d1c9905a6c37d2f7057f1eda8dac291ad0
davidkuchar <davidkuchar@gmail.com>
2012-11-19 12:37:03 -0800

    added conversion values to adwords
    added bing ads support
    added kissmetrics support

--- metron/templatetags/metron_tags.py
+++ metron/templatetags/metron_tags.py
@@ -9,28 +9,43 @@ register = template.Library()
 
 @register.simple_tag(takes_context=True)
 def analytics(context):
-    content = ""
-    for kind, codes in getattr(settings, "METRON_SETTINGS", {}).items():
-        code = codes.get(settings.SITE_ID)
-        if code is not None:
-            t = template.loader.get_template("metron/_%s.html" % kind)
-            content += t.render(template.Context({
-                "code": code,
-                "user": context["user"],
-                "actions": activity.all(context["request"], kind)
-            }))
-    return content
+	content = ""
+	for kind, codes in getattr(settings, "METRON_SETTINGS", {}).items():
+		code = codes.get(settings.SITE_ID)
+		if code is not None:
+			t = template.loader.get_template("metron/_%s.html" % kind)
+			content += t.render(template.Context({
+				"code": code,
+				"user": context["user"],
+				"actions": activity.all(context["request"], kind)
+			}))
+	return content
 
 
 @register.simple_tag(takes_context=True)
-def adwords_conversion(context, key):
-    content = ""
-    page_ids = getattr(settings, "METRON_ADWORDS_SETTINGS", {}).get(key)
-    if page_ids:
-        t = template.loader.get_template("metron/_adwords_conversion.html")
-        content = t.render(template.Context({
-            "conversion_id": page_ids["conversion_id"],
-            "conversion_format": page_ids["conversion_format"],
-            "conversion_label": page_ids["conversion_label"]
-        }))
-    return content
+def adwords_conversion(context, key, conversion_value=0):
+	content = ""
+	page_ids = getattr(settings, "METRON_ADWORDS_SETTINGS", {}).get(key)
+	if page_ids:
+		t = template.loader.get_template("metron/_adwords_conversion.html")
+		content = t.render(template.Context({
+			"conversion_id": page_ids["conversion_id"],
+			"conversion_format": page_ids["conversion_format"],
+			"conversion_label": page_ids["conversion_label"],
+			"conversion_value": conversion_value
+		}))
+	return content
+
+@register.simple_tag(takes_context=True)
+def bingads_conversion(context, key, conversion_value=0):
+	content = ""
+	page_ids = getattr(settings, "METRON_BINGADS_SETTINGS", {}).get(key)
+	if page_ids:
+		t = template.loader.get_template("metron/_bingads_conversion.html")
+		content = t.render(template.Context({
+			"account_id": page_ids["account_id"],
+			"domain_id": page_ids["domain_id"],
+			"action_id": page_ids["action_id"],
+			"conversion_value": conversion_value
+		}))
+	return content
