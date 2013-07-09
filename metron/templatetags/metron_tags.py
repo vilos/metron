8e151a56feba9690d45cb84038552974611c5dc1
Viliam Segeda <viliam.segeda@gmail.com>
2013-07-09 12:49:27 +0200

    convert tabs to spaces

--- metron/templatetags/metron_tags.py
+++ metron/templatetags/metron_tags.py
@@ -24,28 +24,28 @@ def analytics(context):
 
 @register.simple_tag(takes_context=True)
 def adwords_conversion(context, key, conversion_value=0):
-	content = ""
-	page_ids = getattr(settings, "METRON_ADWORDS_SETTINGS", {}).get(key)
-	if page_ids:
-		t = template.loader.get_template("metron/_adwords_conversion.html")
-		content = t.render(template.Context({
-			"conversion_id": page_ids["conversion_id"],
-			"conversion_format": page_ids["conversion_format"],
-			"conversion_label": page_ids["conversion_label"],
-			"conversion_value": conversion_value
-		}))
-	return content
+    content = ""
+    page_ids = getattr(settings, "METRON_ADWORDS_SETTINGS", {}).get(key)
+    if page_ids:
+        t = template.loader.get_template("metron/_adwords_conversion.html")
+        content = t.render(template.Context({
+            "conversion_id": page_ids["conversion_id"],
+            "conversion_format": page_ids["conversion_format"],
+            "conversion_label": page_ids["conversion_label"],
+            "conversion_value": conversion_value
+        }))
+    return content
 
 @register.simple_tag(takes_context=True)
 def bingads_conversion(context, key, conversion_value=0):
-	content = ""
-	page_ids = getattr(settings, "METRON_BINGADS_SETTINGS", {}).get(key)
-	if page_ids:
-		t = template.loader.get_template("metron/_bingads_conversion.html")
-		content = t.render(template.Context({
-			"account_id": page_ids["account_id"],
-			"domain_id": page_ids["domain_id"],
-			"action_id": page_ids["action_id"],
-			"conversion_value": conversion_value
-		}))
-	return content
+    content = ""
+    page_ids = getattr(settings, "METRON_BINGADS_SETTINGS", {}).get(key)
+    if page_ids:
+        t = template.loader.get_template("metron/_bingads_conversion.html")
+        content = t.render(template.Context({
+            "account_id": page_ids["account_id"],
+            "domain_id": page_ids["domain_id"],
+            "action_id": page_ids["action_id"],
+            "conversion_value": conversion_value
+        }))
+    return content
